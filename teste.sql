b06c24ee483aa39da7924545488af732f732e868
André Feuzer <andre.heydran@gmail.com>
2022-07-06 20:18:33 -0300

    ideias de logo e escrita

--- teste.sql
+++ teste.sql
@@ -1,4 +1,34 @@
 CREATE TABLE Usuario(
-cod_usuario SERIAL PRIMARY KEY,
-nome_usuario VARCHAR()
-)
+	cod_usuario SERIAL PRIMARY KEY,
+	nome_usuario VARCHAR(30),
+	email_usuario VARCHAR(30),
+	senha_usuario VARCHAR(60)
+);
+
+CREATE TABLE Renda (
+    cod_renda INTEGER PRIMARY KEY,
+    val_renda FLOAT,
+    data_renda DATE,
+    tipo_renda INTEGER,
+    desc_renda VARCHAR(300),
+    data_final_renda DATE,
+    situacao_renda BOOLEAN,
+    cod_usuario INTEGER,
+    cod_usuario INTEGER
+	FOREIGN KEY (cod_usuario) REFERENCES Usuario(cod_usuario)
+);
+
+CREATE TABLE Despesa (
+    cod_despesa INTEGER PRIMARY KEY,
+	tipo_despesa INTEGER,
+    val_despesa FLOAT,
+    data_despesa DATE,
+    data_final_despesa DATE,
+    situacao_despesa BOOLEAN,
+    desc_despesa VARCHAR(300),
+    cod_usuario INTEGER,
+    data_vencimento_despesa DATE,
+    cod_usuario INTEGER
+	FOREIGN KEY (cod_usuario) REFERENCES Usuario(cod_usuario)
+);
+
