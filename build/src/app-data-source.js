cd30d7f9b8ef7e03eb38955da0d22e7de266f40a
André Feuzer <andre.heydran@gmail.com>
2022-09-07 15:30:29 -0300

    atualizando data source

--- build/src/app-data-source.js
+++ build/src/app-data-source.js
@@ -7,13 +7,16 @@ var Income_1 = require("./entity/Income");
 var Expenses_1 = require("./entity/Expenses");
 var myDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "ec2-44-207-253-50.compute-1.amazonaws.com",
     port: 5432,
-    username: "postgres",
-    password: "1234",
-    database: "dbControleSe",
+    username: "wuyrutizvjdnqe",
+    password: "2f982159cab6aaeb310f2c4e684ef261fdfa60039b3a83ce0c9a16fb6616f4e5",
+    database: "d91dhaif9e9kd5",
     entities: [User_1.User, Income_1.Income, Expenses_1.Expenses],
     logging: false,
-    synchronize: true
+    synchronize: true,
+    migrations: [],
+    subscribers: [],
+    ssl: { rejectUnauthorized: false }
 });
 exports.myDataSource = myDataSource;
