5c3b33b40332bd7b7df79abab5ccd213d742e453
Andre <andre.heydran@gmail.com>
2022-09-15 19:32:51 -0300

    busca das walltes

--- src/routes/walletRoutes.ts
+++ src/routes/walletRoutes.ts
@@ -14,12 +14,12 @@ router.post("/new", async function (req: Request, res: Response) {
 })
 
 
-router.get("/query", async (req: Request, res: Response) => {
+router.get("/query/:id", async (req: Request, res: Response) => {
     const wallets = await req.app.get("myDataSource").getRepository(Wallet).query(`
         select walletCode from wallet_users where userCode == $1 
         inner join Wallet on Wallet.walletCode == wallet_users.walletCode
         
-    `)
+    `,[req.parans.id])
     console.log(wallets)
 
 
