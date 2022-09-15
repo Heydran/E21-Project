f6f70772fe6f720e75c64f481dbe09b6c57d4464
Andre <andre.heydran@gmail.com>
2022-09-15 19:54:04 -0300

    busca das walltes

--- src/routes/walletRoutes.ts
+++ src/routes/walletRoutes.ts
@@ -14,7 +14,7 @@ router.post("/new", async function (req: Request, res: Response) {
 })
 
 
-router.get("/query", async (req: Request, res: Response) => {res.send(req.params.id)
+router.get("/query/:id", async (req: Request, res: Response) => {res.send(req.params.id)
 })
 
 
