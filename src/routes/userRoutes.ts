0cee6016668c0717fe8d8362d0870beefb0189c0
Andre <andre.heydran@gmail.com>
2022-09-21 21:41:47 -0300

    update generico

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -97,7 +97,7 @@ router.post("/login", async (req: Request, res: Response) => {
 
             
 
-            token = await sign(result, "segredo")
+            token = await sign(result, "segredo",{ expiresIn: 6000000})
             console.log({token});
             return res.json({token})
         })}
