c340b2a827d31d0cd120c09d3857af0f311b1da8
Andre <andre.heydran@gmail.com>
2022-09-30 18:41:42 -0300

    update generico

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -5,32 +5,32 @@ import { Expense } from "./entity/Expense"
 import { Wallet } from "./entity/Wallet"
 import { WalletUsers } from './entity/WalletUsers'
 import { Parcel } from "./entity/Parcel"
-const myDataSource = new DataSource({
-    type: "postgres",
-    host: "ec2-44-207-253-50.compute-1.amazonaws.com",
-    port: 5432,
-    username: "wuyrutizvjdnqe",
-    password: "2f982159cab6aaeb310f2c4e684ef261fdfa60039b3a83ce0c9a16fb6616f4e5",
-    database: "d91dhaif9e9kd5",
-    entities: [ User, Income, Expense, Wallet, WalletUsers, Parcel],
-    logging: false,
-    synchronize: true,
-    migrations: [],
-    subscribers: [],
-    ssl: { rejectUnauthorized: false }
-})
-
 // const myDataSource = new DataSource({
 //     type: "postgres",
-//     host: "localhost",
+//     host: "ec2-44-207-253-50.compute-1.amazonaws.com",
 //     port: 5432,
-//     username: "postgres",
-//     password: "1234",
-//     database: "postgres",
-//     entities: [User, Income, Expense, Wallet, WalletUsers, Parcel],
+//     username: "wuyrutizvjdnqe",
+//     password: "2f982159cab6aaeb310f2c4e684ef261fdfa60039b3a83ce0c9a16fb6616f4e5",
+//     database: "d91dhaif9e9kd5",
+//     entities: [ User, Income, Expense, Wallet, WalletUsers, Parcel],
 //     logging: false,
-//     synchronize: true
+//     synchronize: true,
+//     migrations: [],
+//     subscribers: [],
+//     ssl: { rejectUnauthorized: false }
 // })
+
+const myDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "1234",
+    database: "postgres",
+    entities: [User, Income, Expense, Wallet, WalletUsers, Parcel],
+    logging: false,
+    synchronize: true
+})
 
 /*
 drop table Parcel;
@@ -45,14 +45,3 @@ drop table "user";
 
 
 export { myDataSource }
-
-
-var launch = {}
-var table
-if (true){}
-table = "inc"
-launch [ table+"Campo" ] = "ola"
-
-launch = {incCampo : "ola"}
-
-*/
